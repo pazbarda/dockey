48f3ab7a2af125e33ea55f36bbc47657a32bbc89
pazbarda <pazbarda@gmail.com>
2022-02-23 23:07:47 +0200

    uploaded files from local Dockey repo

---
+++ src/test/java/com/pazbarda/dockey/DockeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pazbarda.dockey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
